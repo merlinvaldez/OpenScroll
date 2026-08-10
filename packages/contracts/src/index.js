87e4337270cb3920a0cfe7f89d4a803d619d794d
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2026-08-10 01:24:43 -0400

    Implement Epic C content platform (packages/contracts/src/index.js)

--- packages/contracts/src/index.js
+++ packages/contracts/src/index.js
@@ -1,2 +1,2 @@
-export { apiEnvelopeSchema, errorEnvelopeSchema, paginationSchema } from "./schemas.js";
+export { apiEnvelopeSchema, connectorRunSchema, errorEnvelopeSchema, ingestionRunSchema, paginationSchema, sourceRegistryRecordSchema, universalContentObjectSchema } from "./schemas.js";
 export { eventCatalog, eventEnvelopeSchema } from "./events.js";
