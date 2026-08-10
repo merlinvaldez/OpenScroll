91e5f565f0dba5f026558879d817d9ad14fc2836
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2026-08-10 03:42:31 -0400

    Implement Epic D rights engine: packages/contracts/src/index.js

--- packages/contracts/src/index.js
+++ packages/contracts/src/index.js
@@ -1,2 +1,2 @@
-export { apiEnvelopeSchema, connectorRunSchema, errorEnvelopeSchema, ingestionRunSchema, paginationSchema, sourceRegistryRecordSchema, universalContentObjectSchema } from "./schemas.js";
+export { apiEnvelopeSchema, attributionNoticeSchema, connectorRunSchema, errorEnvelopeSchema, ingestionRunSchema, licenseOntologySchema, openLicenseGateDecisionSchema, paginationSchema, rightsReviewCaseSchema, rightsSafeDownloadPolicySchema, sourceRegistryRecordSchema, universalContentObjectSchema } from "./schemas.js";
 export { eventCatalog, eventEnvelopeSchema } from "./events.js";
